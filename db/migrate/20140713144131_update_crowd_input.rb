2f7162e3ac3cf2f7033831898ec1f4b4ad05e794
Akirato Setiawan <akirato.setiawan@gmail.com>
2014-07-13 21:41:57 +0700

    init

---
+++ db/migrate/20140713144131_update_crowd_input.rb
@@ -0,0 +1,12 @@
+class UpdateCrowdInput < ActiveRecord::Migration
+  def change
+	CrowdInput.find(:all).each do |f|
+		currentIDKelurahan = Tps.where(:id => f.tps_id).kelurahan_id
+		currentIDTps = Tps.where(:id => f.tps_id).tps_id
+		
+		idTpsBaru = TpsBaru.where(:kelurahan_id => currentIDKelurahan).where(:tps_id => currentIDTps).id
+		
+		f.update_attribute :tps_id, idTpsBaru
+    end
+  end
+end
