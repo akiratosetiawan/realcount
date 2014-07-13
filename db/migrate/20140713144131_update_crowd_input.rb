8ad3346cdb3573c4e3a40ec57bc3eaf10d3684b2
Adianto Wibisono <a.wibisono@uva.nl>
2014-07-13 20:09:36 +0200

    Rewrite migration update code, trim zeros di tps lama

--- db/migrate/20140713144131_update_crowd_input.rb
+++ db/migrate/20140713144131_update_crowd_input.rb
@@ -1,12 +1,24 @@
 class UpdateCrowdInput < ActiveRecord::Migration
   def change
+        counter  = 0;
 	CrowdInput.all.each do |f|
-		currentIDKelurahan = Tps.where(:id => f.tps_id).kelurahan_id
-		currentIDTps = Tps.where(:id => f.tps_id).tps_id
-		
-		idTpsBaru = TpsBaru.where(:kelurahan_id => currentIDKelurahan).where(:tps_id => currentIDTps).id
-		
-		f.update_attribute :tps_id, idTpsBaru
-    end
+	   	tpsLama = TpsLama.where(:id =>f.tps_id).first
+
+		#Trim leading zero in tps lama
+                kelurahan_id = tpsLama.kelurahan_id.to_i
+		tps_id = tpsLama.tps_id
+
+                tpsBaru = Tps.where(:kelurahan_id => "#{kelurahan_id}", :tps_id => tps_id).first 
+		if tpsBaru == nil
+		    print "\nNo record found for desa #{tpsLama.desa}, kelurahan #{tpsLama.kelurahan_id}, tps #{tpsLama.tps_id}"
+		    f.update_attribute :tps_id,   0
+		else
+		    f.update_attribute :tps_id, tpsBaru.id 
+		end
+		counter += 1;
+		if counter % 100 == 0
+			print "\nUpdating records # ", counter, " ..."
+		end
+    	end
   end
 end
