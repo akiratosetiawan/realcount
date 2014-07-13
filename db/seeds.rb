dea04b3c455328c0eb109b2be07ada67017ae9ff
Adianto Wibisono <a.wibisono@uva.nl>
2014-07-13 20:08:14 +0200

    Implement @rulimanurung's code, use @fikriauliya's group insert per 100 records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,38 @@
 require 'csv'
 
-indexTPS = Array.new(500000, 0)
 
-CONN = ActiveRecord::Base.connection
 
-puts "Inserting new TPS data"
+inserts = []
 
+total_tps = 0
+puts "Reading new TPS data from @fajran"
 #use the newdata file from @fajran 
 CSV.foreach(File.path("db/newdata")) do |col|
-	
-	#check the degree, proceed if only 4
-	if col[2] == '4'
-		#increment the counter for index
-		indexTPS[col[1].to_i] += 1
-		
-		#filling the data
-		desa = col[3]
-		kelurahan_id = col[1]
-		tps_id = indexTPS[col[1].to_i].to_s
-		
-		#prepare for inserting to database
-		sql = "INSERT INTO tps_barus(desa, kelurahan_id, tps_id) VALUES ('#{desa}', '#{kelurahan_id}', '#{tps_id}')"
-		
-		CONN.execute(sql)
-	end
+        
+        #check the degree, proceed if only 4
+        if col[2] == '4'
+                
+                #filling the data
+                desa = col[3]
+                kelurahan_id = col[0]
+                jumlah_tps = col[4].to_i
+
+                for tps_id in 1..jumlah_tps
+                   #prepare for inserting to database
+                   inserts.push("('#{desa}', '#{kelurahan_id}' , '#{tps_id}')")
+                end
+		total_tps += jumlah_tps
+        end
 end
 
-puts "Inserted new TPS data"
+puts "Jumlah tps yang akan diinsert", total_tps
+CONN = ActiveRecord::Base.connection
+
+puts "Inserting TPS data"
+slices = inserts.each_slice(100).to_a
+
+slices.each do |slice|
+  sql = "INSERT INTO tps_barus(desa, kelurahan_id, tps_id) VALUES #{slice.join(',')}"
+  CONN.execute(sql)
+end
+puts "Inserted TPS data"
