b9914365df062276904e1a4c20174f77cce68935
Akirato Setiawan <akirato.setiawan@gmail.com>
2014-07-13 21:53:20 +0700

    init

--- app/models/tps.rb
+++ app/models/tps.rb
@@ -1,4 +1,5 @@
 class Tps < ActiveRecord::Base
+  self.table_name = "tps_barus"
   def c1_url
     return "http://scanc1.kpu.go.id/viewp.php?f=#{self.kelurahan_id}#{self.tps_id.rjust(3, '0')}04.jpg"
   end
